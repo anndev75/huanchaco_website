32adf065446a4fea562b8f8591b34ce8b553630e
Luis Blas <lfvb2152@gmail.com>
2025-04-02 13:01:15 -0500

    primera edición

--- src/components/fullscreen_header.jsx
+++ src/components/fullscreen_header.jsx
@@ -1,7 +1,22 @@
+import './fullscreen_header.css'
+import escudo from '../assets/Escudo_Huanchaco.png'
+
+
+
+
+
 function fullscreen_header(){
     return (
-        <div>
-            Hello
+        <div id="MainHeader">
+            <div className="left">
+                CENTRAL DE MONITOREO
+            </div>
+            <div className='escudo'>
+                <img src={escudo}/>
+            </div>
+            <div className="right">
+                MUNICIPALIDAD DISTRITAL DE HUANCHACO
+            </div>
         </div>
     )
 }
